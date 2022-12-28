79a79ea9526b07be7badbd068be6557db073c46d
Prakki Rama <ramadatta.88@gmail.com>
2022-12-28 14:37:29 +0800

    Update pipeline.snakefile2.py

--- snakemake/pipeline.snakefile2.py
+++ snakemake/pipeline.snakefile2.py
@@ -1,27 +1,32 @@
 # program to run Species and MLST
 
-#SAMPLES, = glob_wildcards('Assembly/{name}_unicycler.gte1kb.contigs.fasta') #Pull files which ends with fasta
+# Extract filenames which ends with ".fasta" under "Plasmids" folder
+SAMPLES, = glob_wildcards('Plasmids/{name}.fasta') 
 
-SAMPLES, = glob_wildcards('Plasmids/{name}.fasta') #Pull files which ends with fasta
-
+# For code clarity, this variable "prokka_output" is used in rule_all ( see line 13: where we use only "prokka_output" variable instead of whole path)
 prokka_output = '2_PROKKA/{sample}_prokka_out/{sample}.gff'
 
-# The input of rule all is the final output of the pipeline.
+# The input for rule all is the final output of rules in the pipeline. 
+# If there are multiple files, then it should be written inside expand command.
+
+# For example, below I have typed "expand(prokka_output, sample=SAMPLES)" which means it will check the final output if 
+# there are "2_PROKKA/{sample}_prokka_out/{sample}.gff" files created for the rule "run_PROKKA".
+
 rule all:
   input: 
     "1_MLST/mlst_log", 
-    expand(prokka_output, sample=SAMPLES), 
-    expand('3_ROARY/{sample}.gff', sample=SAMPLES), 
-    "3_ROARY/3_ROARY_OUTPUT/summary_statistics.txt"
+    expand(prokka_output, sample=SAMPLES), # Since this output have many files we use expand command
+    expand('3_ROARY/{sample}.gff', sample=SAMPLES),  
+    "3_ROARY/3_ROARY_OUTPUT/summary_statistics.txt" # Since this output does not have many files
+    
+ # Then comes all other rules     
 
 rule run_MLST:
-   # priority: 1
     input: expand("Plasmids/{sample}.fasta", sample=SAMPLES) # Need to specify assemblies with a wildcard
-    output: "1_MLST/mlst_log" # Need to specify MLST output with a wildcard
+    output: "1_MLST/mlst_log" # Need to specify MLST output with a wildcard and this is the output we have given in rule all
     shell: "mlst {input} >> {output}"
 
 rule run_PROKKA:
-   # priority: 2
     input: expand("Plasmids/{sample}.fasta", sample=SAMPLES) # Need to specify assemblies with a wildcard
     output: 
       "2_PROKKA/{sample}_prokka_out/{sample}.gff"
@@ -29,17 +34,15 @@ rule run_PROKKA:
     shell: "prokka2 --force --cpus {threads} --outdir 2_PROKKA/{wildcards.sample}_prokka_out --prefix {wildcards.sample} {input} >>2_PROKKA/prokka_log 2>>2_PROKKA/prokka_error"
 
 rule cp_PROKKA_gffs:
-  #priority: 3
   input: expand("2_PROKKA/{sample}_prokka_out/{sample}.gff", sample=SAMPLES)
   params:
     outdir= "3_ROARY" # For some reason instead of using {output}, using {params.outdir} worked in shell command
-  output: "3_ROARY/{sample}.gff"
+  output: "3_ROARY/{sample}.gff" # Need to specify ROARY output and this is the output we have given in rule all
   shell: "cp {input} {params.outdir}"
 
 rule run_ROARY:
-    #priority: 4
-    input: expand("3_ROARY/{sample}.gff", sample=SAMPLES) # Need to specify assemblies with a wildcard
-    output: "3_ROARY/3_ROARY_OUTPUT/summary_statistics.txt" # Need to specify MLST output with a wildcard
+    input: expand("3_ROARY/{sample}.gff", sample=SAMPLES) # Need to specify gffs with a wildcard
+    output: "3_ROARY/3_ROARY_OUTPUT/summary_statistics.txt" # Need to specify ROARY output and this is the output we have given in rule all 
     params:
       outdir = "3_ROARY/3_ROARY_OUTPUT"
     shell:
